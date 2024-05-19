d0efdea5ce2214b3b1848111a6cb3ee882fe44f0
Michaili K <git@michaili.dev>
2024-05-19 02:36:38 +0200

    Fix indentation & remove duplicate null audio sink

--- hosts/michaili-fortress/audio.nix
+++ hosts/michaili-fortress/audio.nix
@@ -22,57 +22,47 @@
       configPackages = [
         (pkgs.writeTextDir "share/pipewire/pipewire.conf.d/10-virtual-sinks.conf" ''
           context.objects = [
-                   {   factory = adapter
-                       args = {
-                          factory.name     = support.null-audio-sink
-                          node.name        = "VNode System"
-                          media.class      = Audio/Sink
-                          object.linger    = true
-                          audio.position   = [ FL FR ]
-                          monitor.channel-volumes = false
-                       }
-                   },
-                   {   factory = adapter
-                       args = {
-                          factory.name     = support.null-audio-sink
-                          node.name        = "VNode Voice"
-                          media.class      = Audio/Sink
-                          object.linger    = true
-                          audio.position   = [ FL FR ]
-                          monitor.channel-volumes = false
-                       }
-                   },
-                   {   factory = adapter
-                       args = {
-                          factory.name     = support.null-audio-sink
-                          node.name        = "VNode Applications"
-                          media.class      = Audio/Sink
-                          object.linger    = true
-                          audio.position   = [ FL FR ]
-                          monitor.channel-volumes = false
-                       }
-                   },
-                   {   factory = adapter
-                       args = {
-                          factory.name     = support.null-audio-sink
-                          node.name        = "VNode Combined"
-                          media.class      = Audio/Sink
-                          object.linger    = true
-                          audio.position   = [ FL FR ]
-                          monitor.channel-volumes = false
-                       }
-                   },
-                   {   factory = adapter
-                       args = {
-                          factory.name     = support.null-audio-sink
-                          node.name        = "VNode Combined"
-                          media.class      = Audio/Sink
-                          object.linger    = true
-                          audio.position   = [ FL FR ]
-                          monitor.channel-volumes = false
-                       }
-                   }
-               ]
+              {   factory = adapter
+                  args = {
+                     factory.name     = support.null-audio-sink
+                     node.name        = "VNode System"
+                     media.class      = Audio/Sink
+                     object.linger    = true
+                     audio.position   = [ FL FR ]
+                     monitor.channel-volumes = false
+                  }
+              },
+              {   factory = adapter
+                  args = {
+                     factory.name     = support.null-audio-sink
+                     node.name        = "VNode Voice"
+                     media.class      = Audio/Sink
+                     object.linger    = true
+                     audio.position   = [ FL FR ]
+                     monitor.channel-volumes = false
+                  }
+              },
+              {   factory = adapter
+                  args = {
+                     factory.name     = support.null-audio-sink
+                     node.name        = "VNode Applications"
+                     media.class      = Audio/Sink
+                     object.linger    = true
+                     audio.position   = [ FL FR ]
+                     monitor.channel-volumes = false
+                  }
+              },
+              {   factory = adapter
+                  args = {
+                     factory.name     = support.null-audio-sink
+                     node.name        = "VNode Combined"
+                     media.class      = Audio/Sink
+                     object.linger    = true
+                     audio.position   = [ FL FR ]
+                     monitor.channel-volumes = false
+                  }
+              }
+          ]
         '')
       ];
     };
