ec6d5a340b876cf53704d56ab18ae2100921bf60
Michaili K <git@michaili.dev>
2024-07-06 14:03:14 +0200

    GNOME

--- hosts/michaili-fortress/desktop.nix
+++ hosts/michaili-fortress/desktop.nix
@@ -1,12 +1,15 @@
 {pkgs, ...}: {
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
   environment.systemPackages = [
     pkgs.vlc
     pkgs.obs-studio
     pkgs.vesktop
     pkgs.handbrake
   ];
+  hardware.pulseaudio.enable = false;
   services = {
-    desktopManager.plasma6.enable = true;
+    xserver.desktopManager.gnome.enable = true;
+    #desktopManager.plasma6.enable = true;
     displayManager.sddm = {
       enable = true;
       wayland.enable = true;
--- hosts/michaili-fortress/users.nix
+++ hosts/michaili-fortress/users.nix
@@ -14,6 +14,16 @@
     };
   };
   home-manager.users.michaili = {pkgs, ...}: {
+    home.pointerCursor = {
+      name = "Adwaita";
+      package = pkgs.gnome.adwaita-icon-theme;
+      size = 24;
+      x11 = {
+        enable = true;
+        defaultCursor = "Adwaita";
+      };
+    };
+
     programs = {
       git = {
         enable = true;
