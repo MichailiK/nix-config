b40930b41804a0bb29111ce9213289339c2ab45e
Michaili K <git@michaili.dev>
2025-03-21 11:39:49 +0100

    use nixpkgs options for OBS

--- hosts/michaili-fortress/desktop.nix
+++ hosts/michaili-fortress/desktop.nix
@@ -2,15 +2,17 @@
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
   environment.systemPackages = [
     pkgs.vlc
-    (pkgs.wrapOBS {
-      plugins = with pkgs.obs-studio-plugins; [
-        obs-pipewire-audio-capture
-        looking-glass-obs
-      ];
-    })
     pkgs.vesktop
     pkgs.handbrake
   ];
+  programs.obs-studio = {
+    enable = true;
+    enableVirtualCamera = true;
+    plugins = with pkgs.obs-studio-plugins; [
+      obs-pipewire-audio-capture
+      looking-glass-obs
+    ];
+  };
   programs.kdeconnect = {enable = true;};
   services = {
     desktopManager.plasma6.enable = true;
