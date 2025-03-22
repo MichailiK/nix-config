1c0c3f558c537dbcace5bb7ef8ab7091eca312c1
Michaili K <git@michaili.dev>
2025-03-22 20:57:13 +0100

    Add wheel group to nix trusted users

--- hosts/michaili-fortress/nix.nix
+++ hosts/michaili-fortress/nix.nix
@@ -31,6 +31,9 @@ in {
     nixPath = [inputFarm.outPath];
     channel.enable = false;
     settings = {
+      trusted-users = [
+        "@wheel"
+      ];
       experimental-features = [
         "nix-command"
         "flakes"
@@ -39,7 +42,7 @@ in {
       flake-registry = "";
     };
   };
-  
+
   # thank u lychee for thanking raf https://github.com/itslychee/config/blob/0719e197b511a389264127dedeb87a2985bea486/modules/nix/settings.nix#L59-L63
   systemd.tmpfiles.rules = lib.mkIf (!config.nix.channel.enable) [
     "R /root/.nix-defexpr/channels - - - -"
