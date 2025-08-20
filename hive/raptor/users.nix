69f9c6a82bec61282849e0c204b101a778039859
Michaili K <git@michaili.dev>
2025-08-20 23:53:58 +0200

    raptor: add marshmallow as user

--- hive/raptor/users.nix
+++ hive/raptor/users.nix
@@ -16,6 +16,13 @@
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILv8G52B8ANAEczyOLd6N15pmasoVde1I9pXajQOeUL5 openpgp:0xF2E43235"
         ];
       };
+      marshmallow = {
+        isNormalUser = true;
+        extraGroups = ["vm-users"];
+        openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICpSjyumudaVID5jTxLvYTSs9AimCWbRkicJVPZV+5GL hydra@media"
+        ];
+      };
     };
     groups = {
       vm-users = {};
