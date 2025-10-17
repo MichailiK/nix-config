0a8045a810d70233f985fe349caf403f9d40fea3
Michaili K <git@michaili.dev>
2025-10-18 01:56:59 +0200

    raptor: expire lychee user

--- hive/raptor/users.nix
+++ hive/raptor/users.nix
@@ -4,28 +4,29 @@
     users = {
       test = {
         isNormalUser = true;
-        extraGroups = ["vm-users"];
+        extraGroups = [ "vm-users" ];
         openssh.authorizedKeys.keys = [
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOct0GpSUGR8eFXiyPF6rHFQ9r97rdH/+rv/GDZnSyqS openpgp:0x5E718B7B"
         ];
       };
       lychee = {
         isNormalUser = true;
-        extraGroups = ["vm-users"];
+        expires = "2025-01-01";
+        extraGroups = [ "vm-users" ];
         openssh.authorizedKeys.keys = [
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILv8G52B8ANAEczyOLd6N15pmasoVde1I9pXajQOeUL5 openpgp:0xF2E43235"
         ];
       };
       marshmallow = {
         isNormalUser = true;
-        extraGroups = ["vm-users"];
+        extraGroups = [ "vm-users" ];
         openssh.authorizedKeys.keys = [
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICpSjyumudaVID5jTxLvYTSs9AimCWbRkicJVPZV+5GL hydra@media"
         ];
       };
     };
     groups = {
-      vm-users = {};
+      vm-users = { };
     };
   };
 }
