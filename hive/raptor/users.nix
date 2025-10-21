68f628c806f99116dbaedee8ae7ee68a2f5f88af
Michaili K <git@michaili.dev>
2025-10-21 18:06:46 +0200

    raptor: delete lychee user

--- hive/raptor/users.nix
+++ hive/raptor/users.nix
@@ -9,14 +9,6 @@
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOct0GpSUGR8eFXiyPF6rHFQ9r97rdH/+rv/GDZnSyqS openpgp:0x5E718B7B"
         ];
       };
-      lychee = {
-        isNormalUser = true;
-        expires = "2025-01-01";
-        extraGroups = [ "vm-users" ];
-        openssh.authorizedKeys.keys = [
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILv8G52B8ANAEczyOLd6N15pmasoVde1I9pXajQOeUL5 openpgp:0xF2E43235"
-        ];
-      };
       marshmallow = {
         isNormalUser = true;
         extraGroups = [ "vm-users" ];
