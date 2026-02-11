fdb4e1accadbb9967cc74a7aa755f0e0524909b5
Michaili K <git@michaili.dev>
2026-02-11 11:35:06 +0100

    presets: make gpg-agent use pcsc & prepare sq migration

--- presets/yubikey.nix
+++ presets/yubikey.nix
@@ -1,30 +1,46 @@
 # For nodes that are expected to have a YubiKey attached & should do operations with them.
 {
+  config,
   pkgs,
-  lib,
+  iliPkgs,
   ...
 }: {
-  # Maybe consider implementing agent forwarding?
-  # https://wiki.gnupg.org/AgentForwarding
   environment.systemPackages = builtins.attrValues {
     inherit
       (pkgs)
       gnupg
+      sequoia-sq
+      sequoia-sqv
       yubikey-manager
       ;
   };
+  # TODO uncomment once the issues with sequoia addressed below are resolved
+  # programs.gnupg.package = iliPkgs.gpg-sequoia-chameleon;
+
   programs.yubikey-touch-detector.enable = true;
 
-  hardware.gpgSmartcards.enable = true;
+  services.pcscd.enable = true;
+
+  # not neccessary as gpg-agent will be interacting with the card via PCSC
+  # hardware.gpgSmartcards.enable = true;
+
   programs.gnupg.agent = {
+    # sequoia-sq's openpgp-card implementation is gated behind a compiler feature-flag
+    # and has an issue ive encountered https://gitlab.com/sequoia-pgp/sequoia-sq/-/issues/610
     enable = true;
+    # https://codeberg.org/openpgp-card/state/issues/3
     enableSSHSupport = true;
   };
-  services.pcscd.enable = true;
-  # hack, see https://wiki.archlinux.org/title/GnuPG#Smartcards
-  # pcscd is only needed for using yubikey-manager, so pcscd can be enabled when needed
-  # systemd.units."pcscd.service".enable = lib.mkOverride 75 false;
 
-  # conflicts with gpg ssh agent
+  # conflicts with other ssh agents
   services.gnome.gcr-ssh-agent.enable = false;
+
+  systemd.user.tmpfiles.users.${config.mich.meta.defaultUser.name}.rules = let
+    file = pkgs.writeText "${config.mich.meta.defaultUser.name}_scdaemon.conf" ''
+      disable-ccid
+      pcsc-shared
+    '';
+  in [
+    "L+ %h/.gnupg/scdaemon.conf - - - - ${file}"
+  ];
 }
