df4d87385616b90f4158c589aa554a442b347d1a
Michaili K <git@michaili.dev>
2026-05-05 00:46:01 +0200

    hive/osprey: fix caddy plugins hash

--- hive/nodes/osprey/caddy.nix
+++ hive/nodes/osprey/caddy.nix
@@ -3,7 +3,7 @@
     enable = true;
     package = pkgs.caddy.withPlugins {
       plugins = ["github.com/mholt/caddy-l4@v0.1.0"];
-      hash = "sha256-/AxtpMmEvYvbxTSOvANv5wRx/6shTYi/l29L7kRTgE4=";
+      hash = "sha256-BCCz41xaWVsfN293VDC2Jo+naEZxurnhXkTHGxZS1g0=";
     };
     virtualHosts = {
       "michai.li" = {
