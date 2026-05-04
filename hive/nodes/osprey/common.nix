385c1e59ea3dffbb85dd2192245b2e0eca5736bd
Michaili K <git@michaili.dev>
2026-05-05 01:38:58 +0200

    hive/osprey: add ssh public keys

--- hive/nodes/osprey/common.nix
+++ hive/nodes/osprey/common.nix
@@ -3,7 +3,10 @@
     ssh = {
       knowNodesPublicKeys = true;
       trustedWithAgentForwarding = true;
-      publicKeys = [];
+      publicKeys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIYgeTANz3rdtM147P2u4JMHeoKapJS+KOnCbOxhbcqr"
+        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDV3HiV7xEWURwT3wYy1mrqGFTe5JdTZoOt6Zhd+MEy+XPyk+ZCNINNItkyqJ0Nm4D9M0yRHei4X/yUdU3HdVJM2draa0gstyFUuxfUZMtrH5Nd27sQMcBwnzOaZTGnd9ODnc1mZ6w3kTeo/yAHDkiVhIcoLCqa/vK/8y1Zrr182ZLpEeXwF2u01CpiRdy/9QhxsCUphdBryYOIgIOPIWLcog2GFGECcCHA7JlI+sJoz4dsOh0t+3HJ6PfvVUaQ4uqzlTyZnyP3hGsqjxVRvBR0i8odk2WCT111zSk+sHjAYmF03A8tSBx58g+h4K2aAHHFsiL9kuMuaKHEUXw2aUGIQBlIL56T7Xx+4fLBSip1n41/3PpPxQMr6c44rYor69zQCWZePxH4vfUur5S78j3usE/JLBnuz1I9FkmLZwau4qIc+MYpiguz7AlX6jL5/WGx5RxLltLYDrJ066OJfqcgNm7in5xBCx2zWvhQ2X/lZ3CuTxs91ZBp9Teoless0BpfJ0XaWtearKyESlsQvIDjjD95+pbf8WG3kiSao5zRkHc6FbESqO83yeQyfNL2tjfdtu58xpHyX3J/2AXvd0vBtJyL8TyhJDFwzFrlqxaH0CzxEzxBlYeVf0iBanVZabi9QwHmrl0siJCCp7sNyiz/rYbyZ1A19XPQYfNLIje+yQ=="
+      ];
     };
   };
 
